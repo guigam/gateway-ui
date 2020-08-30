678e30047d71f99fa646ff478544a92241e0cca2
Mahdi guiga <mahdiguiga@MacBook-Pro-de-Mahdi.local>
2020-08-30 10:12:30 -0400

    correction

--- src/App.js
+++ src/App.js
@@ -10,9 +10,22 @@ import Home from './Views/Home'
 import About from './Views/About'
 import Product from './Views/Product'
 import MenuEnvironement from './Components/MenuEnvironement';
+import git from 'isomorphic-git'
+// or
+// import * as git from 'isomorphic-git'
+// or
+import {plugins, clone, commit, push} from 'isomorphic-git'
 
 function App() {
+
+    const dir = path.join(process.cwd(), 'test-clone')
+    git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs' }).then(console.log)
+    await fs.promises.writeFile('/tutorial/README.md', `# TEST`)
+    await git.add({ fs, dir: '/tutorial', filepath: 'README.md' })
+    console.log('done')
     return (
+
+
         <div className="relative pb-10 min-h-screen">
             <Router>
 
